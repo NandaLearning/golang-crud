91e1e886fd7756e4154eb5b0ed67f2c12a0b67a1
nanda <nanda@gmail.com>
2024-05-21 14:16:15 +0700

    perubahan dikit

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"latihan-go-crud/config"
 	"latihan-go-crud/routes"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +14,12 @@ func main() {
 	config.InitDB()
 
 	routes.SiswaRoutes(router)
-	router.Run()
+
+	// Fungsi yang diekspor sebagai titik masuk aplikasi
+	Handler := router.Handler()
+
+	// Memulai server HTTP menggunakan fungsi Handler yang diekspor
+	if err := http.ListenAndServe(":8080", Handler); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
